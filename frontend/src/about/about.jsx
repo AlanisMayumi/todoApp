bfee5c21cc56f4308eff176d6fe46e160fbde4d0
AlanisMayumi <mayumi.alanis@gmail.com>
2019-02-11 21:24:43 -0200

    backend_frontend

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name='Sobre' small='Nós' />
+
+        <h2>Nossa História</h2>
+        <p>Desde 2018 anotando suas tarefas para ajudá-lo no cotidiano</p>
+        <h2>missão e Visão</h2>
+        <p>Nossa missão é ajudar as pessoas a organizarem mentalmente suas tarefas de forma mais eficiente, de modo a atingir seus objetivos</p>
+        <h2>Imprensa</h2>
+        <p>Não temos imprensa</p>
+    </div>
+)
